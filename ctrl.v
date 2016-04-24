db8bec2b68bb91c320e2105b178549f6e34efefd
Jongoh Jeong <jong73060@hotmail.com>
2016-04-24 13:57:12 -0400

    ctrl update

--- ctrl.v
+++ ctrl.v
@@ -7,7 +7,7 @@
 
 `timescale 1ns / 1ns
 
-module ctrl(jctrl, jrctrl, memwrite, memread, memtoreg, ALUop, instr);
+module ctrl(jctrl, jrctrl, memWrite, memRead, memToReg, ALUop, instr);
 
 //−−−−−−−−−−−−−Input Ports−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 
@@ -17,9 +17,9 @@ input [3:0] instr;
 
 output jctrl;
 output jrctrl;
-output memwrite;
-output memread;
-output memtoreg;
+output memWrite;
+output memRead;
+output memToReg;
 output [2:0]ALUop;
 
 //−−−−−−−−−−−−−Input ports Data Type−−−−−−−−−−−−−−−−−−−
@@ -30,9 +30,9 @@ wire [3:0] instr;
 // Output port can be a storage element (reg) or a wire
 reg jctrl;
 reg jrctrl;
-reg memwrite;
-reg memread;
-reg memtoreg;
+reg memWrite;
+reg memRead;
+reg memToReg;
 reg [2:0]ALUop;
 
 //−−−−−−----−-−−−−−−Instructions---−−−−−−−−−−−−−−−--−−−
@@ -40,36 +40,48 @@ always @ instr
 begin
 	jctrl = 0;
 	jrctrl = 0;
-	memwrite = 0;
-	memread = 0;
-	memtoreg = 0;
-	ALUop = 0;
-	if (instr == 0) begin	//add
-		ALUop = 3'b111;
-	end if (instr == 2) begin	//nand
-		ALUop = 3'b001;
-	end if (instr == 4) begin	//slt_0
-		ALUop = 3'b010;
-	end if (instr == 5) begin	//slt_1
-		ALUop = 3'b010;
-	end if (instr == 6) begin	//sl
-		ALUop = 3'b011;
-	end if (instr == 7) begin	//sr
-		ALUop = 3'b100;
-	end if (instr == 8) begin	//lw
-		memread = 1;
-		memtoreg = 1;
-	end if (instr == 9) begin 	//sw
-		memwrite = 1;
-	end if (instr == 10) begin	//addi (didn't add to excel sheet)
-		ALUop = 3'b111;
-	end if (instr == 11) begin	//jr
-		jrctlr = 1;
-	end if (instr == 12) begin	//beq	
-		alu_op = 3'b101;
-		jctrl = 1;
-	end if (instr == 13) begin //jal
-		jctrl = 1;	
+	memWrite = 0;
+	memRead = 0;
+	memToReg = 0;
+	ALUop = 3'b000;
+	if (instr == 4'b0000) 
+			begin	//add
+				ALUop = 3'b000;
+		end if (instr == 4'b0010) 
+			begin	//nand
+				ALUop = 3'b001;
+		end if (instr == 4'b0100) 
+			begin	//slt_0
+				ALUop = 3'b010;
+		end if (instr ==4'b0101) 
+			begin	//slt_1
+				ALUop = 3'b010;
+		end if (instr == 4'b0110) 
+			begin	//sl
+				ALUop = 3'b011;
+		end if (instr == 4'b0111) 
+			begin	//sr
+				ALUop = 3'b100;
+		end if (instr == 4'b1000) 
+			begin	//lw
+				memRead = 1;
+				memToReg = 1;
+		end if (instr == 4'b1001) 
+			begin //sw
+				memWrite = 1;
+		end if (instr == 4'b1010) 
+			begin	//addi (didn't add to excel sheet)
+				ALUop = 3'b101;
+		end if (instr == 4'b1011) 
+			begin	//jr
+				jrctlr = 1;
+		end if (instr == 4'b1100) 
+			begin	//beq	
+				ALUop = 3'b101;
+			jctrl = 1;
+		end if (instr == 4'b1110) 
+			begin //jal
+				jctrl = 1;	
+		end
 	end
-end
 endmodule
