7b48129dd9b9d46c51bdb7b479b56db7b51fa144
Jongoh Jeong <jong73060@hotmail.com>
2016-04-24 15:17:04 -0400

    latex

--- main_tb.v
+++ main_tb.v
@@ -20,33 +20,34 @@ module main_tb();
 
 //?????????????Input Ports?????????????????????????????
 // inputs to the DUT are reg type
-reg [4:0]data1;
-
+reg [4:0]reg_data1;
+reg reg_clk;
 //?????????????Output Ports????????????????????????????
 // outputs from the DUT are wire type
-wire [7:0]result;
+wire [7:0]wire_result;
 
 //??????----?-??????Instructions---???????????????--???
 
 // instantiate the DUT using named instantiation
-signext5 signext(result, data1);
+signext5 signext(wire_result, reg_data1);
 
 //initial blocks are sequential and start at time 0
 initial
-begin
+	reg_clk = 0;
+	always $1 clk = ~clk;
 
-	
+begin	
 	
 	$display("<<Starting the Simulation>>");
-	data1 = 5'b10101;
-	$display("data1: %b result: %b", data1, result);
-	$display("data1: %d result: %d", data1, result);
+	reg_data1 = 5'b10101;
+	$monitor("data1: %b result: %b", reg_data1, wire_result);
+	$monitor("data1: %d result: %d", reg_data1, wire_result);
 end
 
 //ctrl(jctrl, jrctrl, memWrite, memRead, memToReg, ALUop, instr);
 //alu(ALUop, data1, data2, zero, result);
-//four2one(out, in0, in1, in2, in3, ctrl);
-//two2one(out, in0, in1, ctrl);
+//mux4_1(out, in0, in1, in2, in3, ctrl);
+//mux2_1(out, in0, in1, ctrl);
 
 endmodule
 		
